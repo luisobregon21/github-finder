b496594380c179f647b0bafb32c49fa798595f7e
Luis Obregon <luis@coquihealth.com>
2022-04-27 20:20:29 -0400

    Testing out class based components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import "./App.css";
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>GIthub Finder</h1>
-    </div>
-  );
+// Class based components
+class App extends Component {
+    // method: function inside a class
+    // render() class cycle method... it's require because it renders the output
+    render() {
+        // What is being returned is JSX, which is JavaScript under the Hood
+        // basiccally seudo HTML code
+        return (
+            <div className="App">
+                <h1>GIthub Finder</h1>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
